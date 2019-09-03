aca84a5648152d5fc0b87c4ff0ac80cbaee8115e
Artur Daschevici <a.daschevici@gmail.com>
2019-09-03 19:38:33 +0100

    SECOND STEP: Create book list

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './index.css'
+import { books } from '../../books.json'
 
-const App = () => <div className="App">Hello Endava</div>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: books.slice(0, 20),
+    }
+  }
+
+  render = () => {
+    const { books } = this.state
+    return (
+      <div className="App">
+        <ul>
+          {books.map(book => (
+            <li>{book.title}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default App
