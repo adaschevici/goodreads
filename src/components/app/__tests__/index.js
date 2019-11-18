19b9644314cd0fc55bec160c17250badc1b3cdd8
Artur Daschevici <a.daschevici@gmail.com>
2019-11-18 22:42:09 +0000

    Checkpoint: Add tests for connected component using redux-mock-store and isomorphic-fetch

--- src/components/app/__tests__/index.js
+++ src/components/app/__tests__/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import fetch from 'isomorphic-fetch'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import App from '../index'
 import renderer from 'react-test-renderer'
+import data from './books'
+import { MemoryRouter } from 'react-router'
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore([thunk])
+
+let store
+
+beforeEach(() => {
+  store = mockStore({
+    appReducer: {
+      books: data,
+      loading: false,
+    },
+  })
+})
 
 test('App is rendered ok', () => {
-  const component = renderer.create(<input />)
-  let _ = component.toJSON()
+  const component = renderer.create(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  )
+  expect(component.toJSON()).toMatchSnapshot()
 })
